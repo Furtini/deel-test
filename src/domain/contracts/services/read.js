8e303eecabecd585ec1a24a6ba40e5ac5c2597c1
Lucas Furtini <lfurtini@gmail.com>
2022-10-14 23:01:22 -0300

    feat: task 1 - adding profile filter when getting contract by id

--- src/domain/contracts/services/read.js
+++ src/domain/contracts/services/read.js
@@ -10,8 +10,17 @@ class ReadService {
     instance = this
   }
 
-  async show(contractId) {
-    const contract = await Contract.findOne({ where: { id: contractId } })
+  async show(contractId, profile) {
+    const profileColumn = getProfileTypeColumn(profile)
+
+    const query = {
+      where: {
+        ['id']: contractId,
+        [profileColumn]: profile.id
+      }
+    }
+
+    const contract = await Contract.findOne(query)
 
     // TODO: Build error classes
     if (!contract) {
@@ -20,6 +29,16 @@ class ReadService {
 
     return contract
   }
+}
+
+/**
+ * Returns the column name according to the profile type
+ * @param {object} profile
+ * @returns 'ClientId' | 'ContractorId'
+ */
+const getProfileTypeColumn = (profile) => {
+  const { type } = profile
+  return type === 'client' ? 'ClientId' : 'ContractorId'
 }
 
 module.exports = new ReadService()
